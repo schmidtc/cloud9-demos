f75be82d963405d35cfcb6c75b7ebc963d4f597a
Matt Weber <matt@mattweber.org>
2011-09-07 15:38:30 -0700

    Formatting updates and documentation on backbone.js cloud9 storage adapeter.

--- todos/js/backbone.c9.storage.js
+++ todos/js/backbone.c9.storage.js
@@ -1,20 +1,24 @@
 /**
  * Backbone Cloud9 Adapter v1.0
+ * Based on Backbone localStorage Adapter
+ * 
  */
 
-// A simple module to replace `Backbone.sync` with *cloud9*-based
-// persistence. Models are given and id if they don't already have one.
+// A simple module to replace `Backbone.sync` with Cloud9 backed
+// persistence. Models are given an id if they don't already have one.
 
 // Generate four random hex digits.
 function S4() {
-   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 };
 
 // Generate a pseudo-GUID by concatenating random hexadecimal.
 function guid() {
-   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + 
+                                                        S4() + S4() + S4());
 };
 
+// Cloud9 backed storage in the specified index and type
 window.C9Store = function(index, type) {
     this.index = index;
     this.type = type;
@@ -22,109 +26,128 @@ window.C9Store = function(index, type) {
 
 _.extend(C9Store.prototype, {
 
-  // Add a model
-  create: function(model, cb) {    
-    if (!model.id) {
-        model.id = model.attributes.id = guid();
-    }
-    
-    var doc = {
-        collection: this.index,
-        type: this.type,
-        id: model.id,
-        source: model
-    };
-    
-    C9.api.index.Document(doc).put(function(data) {
-        if (data.ok) {
-            cb(model);
-        } else {
-            cb("");
+    // Add a model
+    create: function(model, cb) {
+        
+        // generate an id if one does not exist
+        if (!model.id) {
+            model.id = model.attributes.id = guid();
         }
-    });
-  },
 
-  // Same as create.
-  update: function(model, cb) {
-      this.create(model, cb);
-  },
+        // cloud9 document config
+        var doc = {
+            collection: this.index,
+            type: this.type,
+            id: model.id,
+            source: model
+        };
 
-  // Retrieve
-  find: function(model, cb) {
-      C9.api.index.Document({
-          collection: this.index, 
-          type: this.type, 
-          id: model.id
-       }).get(function(data) {
-           cb(data);
-       });
-  },
-
-  // Return the array of all models currently in storage.
-  // default to 50 items returned.
-  findAll: function(cb) {
-    var qry = C9.api.query.queryString('*');
-    var cloud9 = C9.api.search.Search({indices:[this.index], types:[this.type]});
-    cloud9.setQuery(qry).size(50).get(function(results) {
-        var all = [];
-        jQuery.each(results.hits.hits, function(idx, result) {
-            all.push(result._source);
+        // index the document using a put operation
+        // the specified index and type will be created
+        // if it does not already exist
+        C9.api.index.Document(doc).put(function(data) {
+            if (data.ok) {
+                cb(model);
+            } else {
+                cb(false);
+            }
         });
-        cb(all);
-    });
-  },
+    },
 
-  // Delete a model from `this.data`, returning it.
-  destroy: function(model, cb) {
-    C9.api.index.Document({
-        collection: this.index, 
-        type: this.type, 
-        id: model.id
-     }).delete(function(data) {
-         if (data.ok) {
-             cb(model);
-         } else {
-             cb("");
-         }
-     });
-  }
+    // Update a model.  For cloud9 this is the same as a create operation
+    update: function(model, cb) {
+        this.create(model, cb);
+    },
+
+    // Get a specific model
+    find: function(model, cb) {
+        C9.api.index.Document({
+            collection: this.index,
+            type: this.type,
+            id: model.id
+        }).get(function(data) {
+            // TODO what happens when doc does not exist?
+            cb(data._source);
+        });
+    },
+
+    // Return the array of all models currently in storage.
+    // default to 50 items returned.
+    // TODO make size configurable
+    findAll: function(cb) {
+        // do wildcard search for all document in the given index and type
+        var qry = C9.api.query.queryString('*');
+        var cloud9 = C9.api.search.Search({
+            indices: [this.index],
+            types: [this.type]
+        });
+        cloud9.setQuery(qry).size(50).get(function(results) {
+            // extract the models from the source
+            var all = [];
+            jQuery.each(results.hits.hits, function(idx, result) {
+                all.push(result._source);
+            });
+            cb(all);
+        });
+    },
+
+    // Delete a model
+    destroy: function(model, cb) {
+        C9.api.index.Document({
+            collection: this.index,
+            type: this.type,
+            id: model.id
+        }).delete(function(data) {
+            if (data.ok) {
+                cb(model);
+            } else {
+                cb(false);
+            }
+        });
+    }
 
 });
 
-// Override `Backbone.sync` to use delegate to the model or collection's
-// *localStorage* property, which should be an instance of `Store`.
-Backbone.sync = function(method, model, options, error) {
-    
+// cloud9Sync delegates to the model or collection's
+// cloud9storage property, which should be an instance of `C9Store`.
+Backbone.cloud9Sync = function(method, model, options, error) {
+
     // Backwards compatibility with Backbone <= 0.3.3
     if (typeof options == 'function') {
-      options = {
-        success: options,
-        error: error
-      };
+        options = {
+            success: options,
+            error: error
+        };
     }
-    
+
     var c9store = model.cloud9storage || model.collection.cloud9storage;
-    
-    var resp = function(doc) {
+
+    var responseCallback = function(doc) {
         if (doc) {
             options.success(doc);
         } else {
             options.error("Record not found");
         }
     }
-    
+
     switch (method) {
-        case "read":    
-            model.id ? c9store.find(model, resp) : c9store.findAll(resp); 
-            break;
-        case "create":  
-            c9store.create(model, resp); 
-            break;
-        case "update":  
-            c9store.update(model, resp); 
-            break;
-        case "delete":  
-            c9store.destroy(model, resp);
-            break;
+    case "read":
+        model.id ? c9store.find(model, responseCallback) : 
+                                        c9store.findAll(responseCallback);
+        break;
+    case "create":
+        c9store.create(model, responseCallback);
+        break;
+    case "update":
+        c9store.update(model, responseCallback);
+        break;
+    case "delete":
+        c9store.destroy(model, responseCallback);
+        break;
     }
 };
+
+// Override 'Backbone.sync' to default to cloud9Sync, 
+// the original 'Backbone.sync' is still available in 'Backbone.ajaxSync'
+Backbone.ajaxSync = Backbone.sync;
+Backbone.sync = Backbone.cloud9Sync;
